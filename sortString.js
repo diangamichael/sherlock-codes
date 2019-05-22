8b52b4ef2f84c32068d43f2d473a595afdc9de55
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-22 08:00:47 +0100

    Sorts a string

---
+++ sortString.js
@@ -0,0 +1,7 @@
+/**
+ *  TODO Write a function called sortString that takes a string of letters and returns a string with the letters sorted in alphabetical order.
+ */
+
+function sortString(str) {
+  return str.split('').sort().join('');
+}
