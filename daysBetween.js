a3e98b0f12769e1b9dfda1ee6dcebee08771b9fc
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-05 16:49:40 +0100

    Find the differences in days between days formats;

---
+++ daysBetween.js
@@ -0,0 +1,11 @@
+/**  
+ * Calculate and return the number of days between two dates as a string.
+ * Dates may be in any combination of ISO, short or long formats.
+*/
+
+function daysBetween (start, end) {
+  startDay = String(new Date(start)).split(' ')[2];
+  endDay = String(new Date(end)).split(' ')[2];
+
+  return String(endDay - startDay);
+}
